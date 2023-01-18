1860c46bcbbf50831b5ffcedb379927f381cbc4c
Robert Jackson <rjackson@evswl135.evs.anl.gov>
2023-01-18 12:13:14 -0600

    ADD: Newest edits to autoencoder scripts

--- scripts/train_aerosol_autoencoder.py
+++ scripts/train_aerosol_autoencoder.py
@@ -5,6 +5,9 @@ import glob as glob
 import numpy as np
 import sys
 import pickle
+import xbatcher
+from xbatcher import BatchSchema
+
 #import ray
 
 from datetime import timedelta, datetime
@@ -16,96 +19,34 @@ from tensorflow.keras import Model
 from tensorflow.keras.regularizers import l2
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from deephyper.problem import HpProblem
-from deephyper.evaluator import Evaluator
-from deephyper.evaluator.callback import LoggerCallback
-from deephyper.search.hps import AMBS
+from xbatcher.loaders.keras import CustomTFDataset
 
-CBSA = "Houston, TX"
-
-air_now_data = glob.glob('/lcrc/group/earthscience/rjackson/epa_air_now/*.csv')
-air_now_df = pd.concat(map(pd.read_csv, air_now_data))
-air_now_df['datetime'] = pd.to_datetime(air_now_df['DateObserved'] + ' 00:00:00')
-air_now_df = air_now_df.set_index('datetime')
-air_now_df = air_now_df.sort_index()
-air_now_df = air_now_df[air_now_df['ParameterName'] == "PM2.5"]
-print(air_now_df['CategoryNumber'].values.min())
-
-lag = int(sys.argv[1])
-
-def get_air_now_label(time):
-    if np.min(np.abs((air_now_df.index - time))) > timedelta(days=1):
-        return np.nan
-    ind = np.argmin(np.abs(air_now_df.index - time))
-    return air_now_df['CategoryNumber'].values[ind]
-
-site = "hou"
-
-def load_data(species):
-    ds = xr.open_mfdataset('/lcrc/group/earthscience/rjackson/MERRA2/%s_reduced/%sCMASS*.nc' % (site, species)).sortby('time')
-    print(ds)
-    times = np.array(list(map(pd.to_datetime, ds.time.values)))
-    x = ds["%sCMASS" % species].values
+def preprocess_dataset(ds, species):
+    x = ds[species].values
     old_shape = x.shape
     scaler = StandardScaler()
     scaler.fit(np.reshape(x, (old_shape[0], old_shape[1] * old_shape[2])))
     x = scaler.transform(
-            np.reshape(x, (old_shape[0], old_shape[1] * old_shape[2])))
+        np.reshape(x, (old_shape[0], old_shape[1] * old_shape[2])))
     x = np.reshape(x, old_shape)
-    inputs = np.zeros((old_shape[0], old_shape[1], old_shape[2], 3))
-    inputs[:, :, :, 0] = x
-    ds.close()
-    if species == "SO4" or species == "DMS" or species == "SO2":
-       inp = "SU"
-    else:
-       inp = species
-    ds = xr.open_mfdataset('/lcrc/group/earthscience/rjackson/MERRA2/%s_reduced/%sFLUXU*.nc' % (site, inp)).sortby('time')
-    x2 = ds["%sFLUXU" % inp].values
-    scaler = StandardScaler()
-    scaler.fit(np.reshape(x2, (old_shape[0], old_shape[1] * old_shape[2])))
-    x2 = scaler.transform(
-            np.reshape(x2, (old_shape[0], old_shape[1] * old_shape[2])))
-    x2 = np.reshape(x2, old_shape)
-    inputs[:, :, :, 1] = x2
-    ds.close()
-    ds = xr.open_mfdataset('/lcrc/group/earthscience/rjackson/MERRA2/%s_reduced/%sFLUXV*.nc' % (site, inp)).sortby('time')
-    x2 = ds["%sFLUXV" % inp].values
-    scaler = StandardScaler()
-    scaler.fit(np.reshape(x2, (old_shape[0], old_shape[1] * old_shape[2])))
-    x2 = scaler.transform(
-            np.reshape(x2, (old_shape[0], old_shape[1] * old_shape[2])))
-    x2 = np.reshape(x2, old_shape)
-    inputs[:, :, :, 2] = x2
-    classification = np.array(list(map(get_air_now_label, times)))
-    where_valid = np.isfinite(classification)
-    inputs = inputs[where_valid, :, :, :]
-    classification = classification[where_valid] - 1
-    if lag > 0:
-        classification = classification[lag:]
-        inputs = inputs[:(-lag), :, : :]
-    print(inputs.shape, classification.shape)
-    y = tf.one_hot(classification, 5).numpy()
-    x_train, x_test, y_train, y_test = train_test_split(
-            inputs, y, test_size=0.20, random_state=3)
-    shape = inputs.shape
-    x_dataset_train = {'input_%sMASS' % species: np.squeeze(x_train[:, :, :, 0]),
-            'input_%sFLUXU' % inp: np.squeeze(x_train[:, :, :, 1]),
-            'input_%sFLUXV' % inp: np.squeeze(x_train[:, :, :, 2])}
-    x_dataset_test = {'input_%sMASS' % species: np.squeeze(x_test[:, :, :, 0]),
-            'input_%sFLUXU' % inp: np.squeeze(x_test[:, :, :, 1]),
-            'input_%sFLUXV' % inp: np.squeeze(x_test[:, :, :, 2])}
+    x = x[:, :, :, np.newaxis]
+    x = tf.image.resize(x, (16, 32)).numpy()
+    ds[species] = xr.DataArray(x, dims=('time', 'x', 'y', 'channel'))
+    return ds
 
-    return x_dataset_train, x_dataset_test, y_train, y_test, shape
+def load_data(species):
+    preproc = lambda x: preprocess_dataset(x, species)
+    ds = xr.open_mfdataset('/Users/rjackson/MERRA2_test/%s*.nc' % species,
+                           preprocess=preproc).sortby('time')
+    x_train, x_test = train_test_split(
+            ds[species], test_size=0.20, random_state=3)
+    return x_train, x_test
 
 
-def classifier_model(shape, the_dict, dataset):
+def classifier_model(shape, the_dict, dataset, species):
     width = shape[2]
     height = shape[1]
-    mpool_1s = []
-    in_layers = []
-    dict_keys = list(dataset.keys())
-    num_variables = len([x for x in dataset.keys()])
-    input1 = Input(shape=(height, width, 1))
+    input1 = Input(shape=(height, width, 1), name=species)
     
     mpool_1 = input1
     for i in range(the_dict['num_layers']):
@@ -118,11 +59,7 @@ def classifier_model(shape, the_dict, dataset):
         else:
             name = "layer_%d" % i
         mpool_1 = MaxPooling2D((2, 2), name=name)(conv2d_1)
-    orig_shape = mpool_1.shape
-    flat_1 = Flatten()(mpool_1)
 
-
-    
     for i in range(the_dict['num_layers']):
         conv2d_1 = Conv2DTranspose(the_dict['num_channels'],
             (2, 2), activation=the_dict['activation'], padding='same',
@@ -137,42 +74,33 @@ def classifier_model(shape, the_dict, dataset):
 
 
 def run(config: dict):
-    x_ds_train = {}
-    x_ds_test = {}
-    y_train = []
-    y_test = []
-    species_list = ['SS', 'SO4', 'SO2', 'OC', 'DU', 'DMS', 'BC']
-    for species in species_list:
-        print(species)
-        x_ds_train1, x_ds_test1, y_train, y_test, shape = load_data(species)
-        x_ds_train.update(x_ds_train1)
-        x_ds_test.update(x_ds_test1)
-    
-    #x_train = np.stack([x for x in x_ds_train.values()], axis=3)
-    #x_test = np.stack([x for x in x_ds_test.values()], axis=3)
-    var_keys = [x for x in x_ds_train.keys()]
-    variable = var_keys[int(sys.argv[2])]
-    x_train = x_ds_train[variable][:, :, :, np.newaxis]
-    x_test = x_ds_test[variable][:, :, :, np.newaxis]
-    with tf.device("/GPU:0"):
-        x_train = tf.image.resize(x_train, (16, 32)).numpy()
-        x_test = tf.image.resize(x_test, (16, 32)).numpy()
-        print(x_train.shape, x_test.shape)
-        print(y_train.shape, y_test.shape)
-        shape = (None, 16, 32, 17)
-        model = classifier_model(shape, config, x_ds_test)
-        model.compile(optimizer=Adam(lr=config["learning_rate"]),
-                loss="mean_squared_error")
-        model.summary()
 
-    # AQI classes inbalanced, need weights
-        class_weight = {0: 1, 1: 2, 2: 20, 3: 90, 4: 510}
-        history = model.fit(
-                x_train, x_train, validation_data=(x_test, x_test),
-             epochs=config["num_epochs"], callbacks=[EarlyStopping(patience=100,
-                 monitor="val_loss")],
-                batch_size=config["batch_size"])
-        model.save('../models/autoencoder-%s-lag-%d' % (variable, lag))
+    x_ds_train, x_ds_test = load_data(sys.argv[1])
+
+    x_ds_train_gen = x_ds_train.batch.generator(
+        input_dims={'x': 16, 'y': 32, 'channel': 1},
+        batch_dims={'time': config["batch_size"]})
+    x_ds_test_gen = x_ds_test.batch.generator(
+        input_dims={'x': 16, 'y': 32, 'channel': 1},
+        batch_dims={'time': config["batch_size"]})
+    batch_scheme = BatchSchema(x_ds_test,
+                               input_dims={'x': 16, 'y': 32, 'channel': 1},
+        batch_dims={'time': config["batch_size"]})
+    batch_scheme.to_json('test.json')
+    shape = x_ds_train.shape
+    species = sys.argv[1]
+    model = classifier_model(shape, config, x_ds_test, species)
+    model.compile(optimizer=Adam(lr=config["learning_rate"]),
+                  loss="mean_squared_error")
+    model.summary()
+    print(x_ds_train_gen[1])
+    dataset_train = CustomTFDataset(x_ds_train_gen, x_ds_train_gen)
+    dataset_test = CustomTFDataset(x_ds_test_gen, x_ds_test_gen)
+    history = model.fit(
+        dataset_train, validation_data=dataset_test,
+        epochs=config["num_epochs"], callbacks=[EarlyStopping(patience=100,
+        monitor="val_loss")])
+    model.save('../models/autoencoder-%s-lag-%d' % (variable, lag))
     return history.history
 
 default_config = {
@@ -185,42 +113,4 @@ default_config = {
         "batch_size": 16,
         "num_layers": 3}
 
-
-#if not ray.is_initialized():
-#    ray.init(num_cpus=128, num_gpus=8, log_to_driver=False)
-
 run(default_config)
-#run_default = ray.remote(num_cpus=16, num_gpus=1)(run)
-#objective_default = ray.get(run_default.remote(default_config))
-
-
-#problem = HpProblem()
-#problem.add_hyperparameter((20, 200), "num_epochs")
-#problem.add_hyperparameter((8, 512, "log-uniform"), "num_dense_nodes")
-#problem.add_hyperparameter((1, 2), "num_layers")
-#problem.add_hyperparameter((1, 8), "num_dense_layers")
-#problem.add_hyperparameter((4, 256, "log-uniform"), "num_channels")
-# Categorical hyperparameter (sampled with uniform prior)
-#ACTIVATIONS = [
-#    "elu", "gelu", "hard_sigmoid", "linear", "relu", "selu",
-#    "sigmoid", "softplus", "softsign", "swish", "tanh",
-#]
-#problem.add_hyperparameter(ACTIVATIONS, "activation")
-#problem.add_hyperparameter((1e-5, 1e-2, "log-uniform"), "learning_rate")
-#problem.add_hyperparameter((8, 256, "log-uniform"), "batch_size")
-
-#method_kwargs = {
-#        "num_cpus": 128,
-#        "num_cpus_per_task": 16,
-#        "callbacks": [LoggerCallback()]
-#    }
-
-#method_kwargs["num_cpus"] = 128
-#method_kwargs["num_gpus"] = 4
-#method_kwargs["num_cpus_per_task"] = 16
-#method_kwargs["num_gpus_per_task"] = 1
-
-#evaluator = Evaluator.create(run, method="ray", method_kwargs=method_kwargs)
-#search = AMBS(problem, evaluator)
-#results = search.search(50)
-#results.to_csv('hpsearch_results_classifieraqi.csv')
